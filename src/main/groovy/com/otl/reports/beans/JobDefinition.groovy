4218964ac424891374f04ec77b7eddba60ad9ce1
balajisridhar <s.balajisridhar@gmail.com>
2015-02-10 18:24:06 +0530

    Added bean files for processing the jobs.

---
+++ src/main/groovy/com/otl/reports/beans/JobDefinition.groovy
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.otl.reports.beans
+
+/**
+ * @author hutchuk
+ *
+ */
+class JobDefinition {
+
+	def jobName
+	def jobID
+	def jobDesc
+	def jobValidity
+	def jobStartDate
+	def jobEndDate
+	def jobFrequency
+	def jobRetry
+	def jobReportFolder
+	def jobToAddress
+	def jobCCAddress
+	def jobBCCAddress
+	def jobSubject
+	def jobMailContent
+	def jobOwner
+	
+	
+	
+ 
+	
+}
---
+++ src/main/groovy/com/otl/reports/beans/JobInstances.groovy
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.otl.reports.beans
+
+/**
+ * @author hutchuk
+ *
+ */
+class JobInstances {
+	
+	def jobInstID
+	def jobInstDesc
+	def jobParentID
+	def jobInstActualStartTime
+	def jobInstRevisedStartTime
+	def jobInstResult
+	def jobInstRetry
+	def jobInstReportLocation
+	def jobInstReportName
+
+	
+	
+}
